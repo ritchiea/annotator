7169e19e4e8ea43db2ac53df818e5649039e8d1a
Andrew Ritchie <ritchiea@gmail.com>
2014-08-28 17:38:58 -0400

    more css

--- annotator.js
+++ annotator.js
@@ -6,12 +6,21 @@
 
     var config = {}
     var annotations = this.annotations = options.annotations || [];
+    var formDisplayed = false;
 
     config.db = options.db;
     config.target = options.target || window.location.href;
     config.selector = options.selector || 'body';
 
     var form = $('<div>', { id: 'annotator-form', class: 'annotator-form'});
+    form.append($('<div>', { class: 'annotator-close', text: 'X' }));
+    form.append($('<div>', { class: 'clearfix' }));
+    form.append($('<input>', { id: 'annotator-body-input', type: 'text' }));
+    form.append($('<input>', { id: 'annotator-name-input', type: 'text' }));
+    form.append($('<input>', { id: 'annotator-email-input', type: 'text' }));
+    form.append($('<input>', { id: 'annotator-submit', type: 'submit' }));
+    form.append($('<div>', { class: 'clearfix' }));
+
     $(config.selector).prepend(form);
 
     this.push = function(annotation) {
@@ -23,9 +32,15 @@
       return JSON.stringify(this.annotations);
     }
 
+    $('.annotator-close').click(function(e) {
+      $(this).parent().fadeOut(100);
+      formDisplayed = false;
+    });
+
     $(config.selector).mousedown(function(e) {
-      if (!e.target.toString().match(/http/)) {
+      if (!e.target.toString().match(/http/) && !formDisplayed) {
         e.preventDefault();
+        formDisplayed = true;
         if (e.which === 1) {
           console.log(e);
           $('.annotator-form').css({top: e.pageY, left: e.pageX }).fadeIn(100)
