fdd9844fe975abf4500880e4c474513127455453
Andrew Ritchie <ritchiea@gmail.com>
2014-08-28 21:25:34 -0400

    switch to text area

--- annotator.js
+++ annotator.js
@@ -18,11 +18,7 @@
 
     fireBaseStore.on('value', function (snapshot) {
       data_from_firebase = snapshot.val();
-      if (Array.isArray(data_from_firebase)) {
-        annotator.annotations = data_from_firebase;
-      } else {
-        annotator.annotations = [];
-      }
+      annotator.annotations = data_from_firebase;
     }, function (errorObject) {
       console.log('The read failed: ' + errorObject.code);
     });
@@ -30,7 +26,7 @@
     var form = $('<div>', { id: 'annotator-form', class: 'annotator-form'});
     form.append($('<div>', { class: 'annotator-close', text: 'X' }));
     form.append($('<div>', { class: 'clearfix' }));
-    form.append($('<input>', { class: 'annotator-body-input', type: 'text', placeholder: 'Comment' }));
+    form.append($('<textarea>', { class: 'annotator-body-input', placeholder: 'Comment' }));
     form.append($('<input>', { class: 'annotator-name-input', type: 'text', placeholder: 'Name' }));
     form.append($('<input>', { class: 'annotator-email-input', type: 'text', placeholder: 'Email' }));
     form.append($('<input>', { class: 'annotator-submit', type: 'submit' }));
@@ -41,9 +37,8 @@
 
     this.push = function(annotation) {
       annotation.target = config.target;
-      this.annotations.push(annotation);
-      updateDb(this.annotations);
-      return this.annotations;
+      updateDb(annotation);
+      return annotation;
     }
 
     this.dataToString = function(options) {
@@ -61,9 +56,7 @@
       var annotationData = {};
       $submit.siblings('input').each(function(index) {
         var $input = $(this);
-        if ($input.attr('class').match(/body/)) {
-          annotationData.body = $input.val();
-        } else if ($input.attr('class').match(/name/)) {
+        if ($input.attr('class').match(/name/)) {
           annotationData.name = $input.val();
         } else if ($input.attr('class').match(/email/)) {
           annotationData.email = $input.val();
@@ -74,6 +67,10 @@
 
       });
 
+      $body = $($submit.siblings('textarea')[0]);
+      annotationData.body = $body.val();
+      $body.val('');
+
       annotator.push(new Annotation(annotationData));
 
       $submit.parent().fadeOut(100);
@@ -81,7 +78,7 @@
     });
 
     function updateDb(data) {
-      fireBaseStore.set(data);
+      fireBaseStore.push(data);
     }
 
     $(config.selector).mousedown(function(e) {
@@ -115,6 +112,7 @@
       this[prop] = properties[prop];
     }
     
+    // added this function before implementing firebase, can be eliminated
     function getGuid() {
       return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
