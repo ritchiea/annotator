6f49e59aa6a4929b6d86586819781e07d3d121cb
Andrew Ritchie <ritchiea@gmail.com>
2014-08-28 20:54:04 -0400

    implement firebase

--- annotator.js
+++ annotator.js
@@ -7,28 +7,42 @@
     var annotator = this;
 
     var config = {}
-    var annotations = this.annotations = options.annotations || [];
     var formDisplayed = false;
 
-    config.db = options.db;
+    config.db = options.db || 'https://brilliant-torch-2878.firebaseio.com/';
     config.target = options.target || window.location.href;
     config.selector = options.selector || 'body';
+
+    var fireBaseStore = new Firebase(config.db);
+    var data_from_firebase = [];
+
+    fireBaseStore.on('value', function (snapshot) {
+      console.log(snapshot.val());
+      if (!( typeof(snapshot) === 'undefined' )) {
+        data_from_firebase = snapshot.val();
+        annotator.annotations = data_from_firebase || [];
+      }
+    }, function (errorObject) {
+      console.log('The read failed: ' + errorObject.code);
+    });
 
     var form = $('<div>', { id: 'annotator-form', class: 'annotator-form'});
     form.append($('<div>', { class: 'annotator-close', text: 'X' }));
     form.append($('<div>', { class: 'clearfix' }));
-    form.append($('<input>', { class: 'annotator-body-input', type: 'text' }));
-    form.append($('<input>', { class: 'annotator-name-input', type: 'text' }));
-    form.append($('<input>', { class: 'annotator-email-input', type: 'text' }));
+    form.append($('<input>', { class: 'annotator-body-input', type: 'text', placeholder: 'Comment' }));
+    form.append($('<input>', { class: 'annotator-name-input', type: 'text', placeholder: 'Name' }));
+    form.append($('<input>', { class: 'annotator-email-input', type: 'text', placeholder: 'Email' }));
     form.append($('<input>', { class: 'annotator-submit', type: 'submit' }));
+    form.append($('<input>', { class: 'annotator-annotated-content', type: 'hidden' }));
     form.append($('<div>', { class: 'clearfix' }));
 
     $(config.selector).prepend(form);
 
     this.push = function(annotation) {
       annotation.target = config.target;
-      annotations.push(annotation);
-      return annotations;
+      this.annotations.push(annotation);
+      updateDb(this.annotations);
+      return this.annotations;
     }
 
     this.dataToString = function(options) {
@@ -52,7 +66,11 @@
           annotationData.name = $input.val();
         } else if ($input.attr('class').match(/email/)) {
           annotationData.email = $input.val();
+        } else if ($input.attr('class').match(/content/)) {
+          annotationData.annotatedContent = JSON.parse($input.val());
         }
+        $input.val('')
+
       });
 
       annotator.push(new Annotation(annotationData));
@@ -61,20 +79,25 @@
       formDisplayed = false;
     });
 
-    function updateDb() {
-      $.ajax({ 
-        url: config.db
-      }).done(function() {
-
-      });
+    function updateDb(data) {
+      fireBaseStore.set(data);
     }
 
     $(config.selector).mousedown(function(e) {
       if (!e.target.toString().match(/http/) && !formDisplayed) {
         e.preventDefault();
+        var annotatedContent = {
+          id: e.target.id,
+          classList: e.target.classList,
+          innerHTML: e.target.innerHTML
+        };
         formDisplayed = true;
         if (e.which === 1) {
-          $('.annotator-form').css({top: e.pageY, left: e.pageX }).fadeIn(100)
+          console.log(e)
+          $('.annotator-form')
+            .css({top: e.pageY, left: e.pageX })
+            .fadeIn(100)
+            .find('input[type="hidden"]').val(JSON.stringify(annotatedContent));
         }
       }
 
@@ -83,7 +106,7 @@
 
   function Annotation(properties) {
     //       EXAMPLE
-    //      { selector: 'h1.title', 
+    //      { annotatedContent: { ... }, 
     //        body: 'Iama annotation', 
     //        author: 'Andrew Ritchie',
     //        email: 'hello@andrewritchie.info'  }
@@ -91,6 +114,15 @@
     for (var prop in properties) {
       this[prop] = properties[prop];
     }
+    
+    function getGuid() {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+      })
+    }
+
+    this.id = getGuid();
 
   }
 
